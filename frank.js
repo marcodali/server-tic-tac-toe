6fc0542b3dc794ce9d98fde88b2c7ee4fd22b87e
Marco Dali <marco@durazno.io>
2023-03-25 13:07:35 -0600

    event 2 completed

--- frank.js
+++ frank.js
@@ -5,6 +5,7 @@ import { Server } from 'socket.io';
 const app = express();
 const server = createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
+const sessions = {};
 
 try {
 
@@ -13,10 +14,39 @@ io.on('connection', (socket) => {
 
     socket.on('PROF_START_CLASS', (res) => {
         console.log('inside PROF_START_CLASS', res.session, res.status);
+        if (res.session in sessions) {
+            // do nothing
+        } else {
+            // create class with empty student list
+            sessions[res.session] = [];
+        }
     });
 
-    socket.on('STUDENT_ENTER_CLASS', (res) => {
+    socket.on('STUDENT_ENTER_CLASS', (res, callback) => {
         console.log('inside STUDENT_ENTER_CLASS', res);
+        if (res.session in sessions) {
+            /**
+             * find the student in the list,
+             * if not present then add it
+             */
+            const student = sessions[res.session]
+                .find(student => student.user === res.user);
+            if (!student) {
+                sessions[res.session].push({
+                    user: res.user,
+                    raisedHand: false,
+                })
+            }
+            callback(sessions[res.session]);
+        } else {
+            const msg = `ERROR: el alumno ${
+                res.user
+            } esta en una session que no existe ${
+                res.session
+            }`;
+            console.error(msg);
+            callback(msg);
+        }
     });
 
     socket.on("disconnecting", () => {
