c7448e5b30563712b53f768d39c56791ccede26b
Marco Dali <marco@durazno.io>
2023-05-01 13:46:11 -0600

    profe santi game

--- game.js
+++ game.js
@@ -2,11 +2,59 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 
+const question = {
+    questionNumber: 4,
+    id: '29n3cxfy7228dl',
+    signature: 'Ciencias Sociales',
+    area: 'Filosofia',
+    topic: 'Marxismo',
+    subtopic: 'Marxismo subtopic',
+    type: 'justification',
+    difficulty: 'easy',
+    text: 'Edson y Edwin charlan en su casa. Los dos han tenido dificultades en la vida, no les ha ido bien últimamente en ningún aspecto. Edson dice que tal vez se deba a la posición de los astros, pues al ser del signo de cáncer, no es un buen momento del año para él. Edwin responde que se debe más a las decisiones que ha tomado, y que, si hay algo ajeno que influye en la toma de sus decisiones, se trata de la propia estructura social que beneficia a unos y perjudica a demasiados.',
+    subtext: '¿A qué noción ética corresponde la discusión de Edwin y Edson?',
+    optionOne: 'Autonomía y heteronomía',
+    optionTwo: 'Capitalismo y Socialismo',
+    optionThree: 'Determinismo y libertad',
+    optionFour: 'Marxismo y Leninismo',
+    correctAnswer: 3,
+    selectedAnswer: ''
+};
+
+const option = [
+    {
+      id: 'A',
+      label: 'Autonomía y heteronomía',
+      value: 'A',
+      selected: false
+    },
+    {
+      id: 'B',
+      label: 'Capitalismo y Socialismo',
+      value: 'B',
+      selected: false
+    },
+    {
+      id: 'C',
+      label: 'Determinismo y libertad',
+      value: 'C',
+      selected: false
+    },
+    {
+      id: 'D',
+      label: 'Marxismo y Leninismo',
+      value: 'D',
+      selected: true
+    }
+  ];
+
 const app = express();
 const server = createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
 const players = {};
+const playersSockets = {};
+const games = {};
 
 try {
 
@@ -26,12 +74,18 @@ io.on('connection', (socket) => {
                 name: res.name,
                 socketID: socket.id,
             }
+            playersSockets[socket.id] = {
+                id: res.id,
+                name: res.name,
+            }
         }
 
         const playersArr = Object.keys(players)
         if (playersArr.length >= 2) {
             const p1 = players[playersArr.pop()];
             const p2 = players[playersArr.pop()];
+            const gameID = Math.random();
+            games[gameID] = { p1, p2};
             io
                 .to(p1.socketID)
                 .emit('START_GAME', {
@@ -39,6 +93,7 @@ io.on('connection', (socket) => {
                     name: p2.name,
                     myRol: 'player1',
                     totalQuestions: 10,
+                    gameID,
                 });
             io
                 .to(p2.socketID)
@@ -47,9 +102,32 @@ io.on('connection', (socket) => {
                     name: p1.name,
                     myRol: 'player2',
                     totalQuestions: 5,
+                    gameID,
                 });
             delete players[p1.id];
             delete players[p2.id];
+        }
+    });
+
+    socket.on('GIVE_ME_QUESTION', (res, callback) => {
+        if (games[res.gameID]
+            && (games[res.gameID].p1.socketID == socket.id
+            || games[res.gameID].p2.socketID == socket.id)
+        ) {
+            callback({
+                question,
+                option,
+            });
+        } else {
+            const msg = `ERROR: el user=${
+                socket.id
+            } con player=${
+                playersSockets[socket.id]
+            } no pertenece al game ${
+                res?.gameID
+            }`;
+            console.error(msg);
+            callback(msg);
         }
     });
 
